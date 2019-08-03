822827717ae02e9c46868f4e36d4cf560f4ec823
Hans Garcia <hansgarcia@Hanss-MacBook-Pro.local>
2019-08-02 22:29:54 -0400

    Finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -3,15 +3,41 @@ class SortedList {
       this.items=[];
       this.length = 0;
     }
-    add(item)
-      {this.items.push(item)
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a,b) => b - a);
+      this.items.reverse();
       this.length++}
-    get(pos)
-      {return this.items[pos]}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    get(position) {
+        return this.items[position]
+      }
+    max() {
+        if(this.items.length === 0){
+          throw new Error("EmptySortedList");
+        }
+        return this.items[this.items.length-1];
+    }
+    min() {
+      if(this.items.length === 0){
+        throw new Error("EmptySortedList");
+      }
+      return this.items[0]
+    }
+    average() {
+      if (this.items.length === 0) {
+        throw new Error("EmptySortedList");
+      }
+      let sum = this.items.reduce((a, b) => a + b, 0)
+      return sum/this.items.length;
+    }
+    sum() {
+      if (this.items.length === 0) {
+        return 0;
+      }
+      let sum = this.items.reduce((a, b) => a + b, 0)
+      return sum;
+      // return this.items[this.items].reduce((a, b) => a + b, 0);
+    }
   };
   
   module.exports = SortedList;
